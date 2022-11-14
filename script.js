22a24a7a71957792d0cb6f9fb406818bd463d308
Cosmin Moisă <cosm33n@gmail.com>
2022-11-14 17:01:40 +0200

    Add toggle functionality

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const panels = document.querySelectorAll('.panel')
 
 panels.forEach(panel => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+  panel.addEventListener('click', () => {
+    if (panel.classList.contains('active')) {
+      panel.classList.remove('active')
+    } else {
+      removeActiveClasses()
+      panel.classList.add('active')
+    }
+  })
 })
 
 function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  panels.forEach(panel => {
+    panel.classList.remove('active')
+  })
 }
